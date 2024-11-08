ca4e95a75c64ed5ff2f781bc44b556ea2844c48b
Jeremy Moyers <jeremy@jeremymoyers.com>
2024-11-07 21:16:20 -0800

    clean index

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,9 @@
 import { filterSchedule } from "./filterSchedule";
 import { parsePDF } from "./parsePDF";
 
+const TEAM_NAME = "7 The Other Team";
+
 const schedule = await parsePDF("./schedule.pdf");
-const games = filterSchedule("7 The Other Team", schedule);
+const games = filterSchedule(TEAM_NAME, schedule);
 
-
-console.log(games)
-
-
-
-
+console.log(games);
